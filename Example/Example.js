a460715a751dad3a1ad5aeece964adf18b14b1bd
Krzysztof Magiera <krzys@swmansion.com>
2017-07-13 17:14:34 +0200

    More fun with animations.

--- Example/Example.js
+++ Example/Example.js
@@ -91,7 +91,6 @@ class PhysicsAnimation extends Animated.Animation {
     this._input = animatedValue;
     this._x = animatedValue.__getValue();
     this._v = animatedValue.__getVelocity();
-    console.log("START", this._x, this._v);
     this._animationFrame = requestAnimationFrame(this.onUpdate.bind(this));
   }
 
@@ -160,6 +159,9 @@ class DraggableBox extends Component {
     this._dragX = new Animated.Value(0);
     this._dragY = new Animated.Value(0);
 
+    this._transX = this._dragX;
+    this._transX = this._dragX.interpolate({ inputRange: [-100, -50, 0, 50, 100], outputRange: [-30, -10, 0, 10, 30]})
+
     this._animationConfig = {
       // forces: [new Gravity(100, 500), new Damping(6)],
       forces: [new Spring(0, 80), new Damping(15), new Spring(100, 100)],
@@ -174,7 +176,6 @@ class DraggableBox extends Component {
       this._dragX.stopAnimation();
       this._dragX.extractOffset();
     } else if (event.nativeEvent.oldState === State.ACTIVE) {
-      console.log("FLAT");
       this._dragX.flattenOffset();
       this._dragX.animate(new PhysicsAnimation(this._animationConfig), () => {
         this._dragX.extractOffset();
@@ -190,10 +191,53 @@ class DraggableBox extends Component {
           onHandlerStateChange={this._onHandlerStateChange}
           id="dragbox">
         <Animated.View style={[styles.box, { transform: [
-          {translateX: this._dragX},
+          {translateX: this._transX},
           /*{{translateY: this._translateY}}*/
         ]}]}/>
       </PanGestureHandler>
+    );
+  }
+}
+
+class TwistableBox extends Component {
+  constructor(props) {
+    super(props);
+    this._gesture = new Animated.Value(0);
+
+    this._rot = this._gesture
+      .interpolate({ inputRange: [-1.2, -1, -0.5, 0, 0.5, 1, 1.2], outputRange: [-0.52, -0.5, -0.3, 0, 0.3, 0.5, 0.52] })
+      .interpolate({ inputRange: [-100, 100], outputRange: ['-100rad', '100rad']});
+
+    this._animationConfig = {
+      forces: [new Spring(0, 120), new Damping(8)],
+    }
+
+    this._onGestureEvent = Animated.event(
+       [{ nativeEvent: { rotation: this._gesture }}],
+    )
+  }
+  _onHandlerStateChange = (event) => {
+    if (event.nativeEvent.state === State.ACTIVE) {
+      this._gesture.stopAnimation();
+      this._gesture.extractOffset();
+    } else if (event.nativeEvent.oldState === State.ACTIVE) {
+      this._gesture.flattenOffset();
+      this._gesture.animate(new PhysicsAnimation(this._animationConfig), () => {
+        this._gesture.extractOffset();
+      });
+    }
+  }
+  render() {
+    return (
+      <RotationGestureHandler
+          {...this.props}
+          onGestureEvent={this._onGestureEvent}
+          onHandlerStateChange={this._onHandlerStateChange}
+          id="dragbox">
+        <Animated.View style={[styles.box, { transform: [
+          {rotate: this._rot},
+        ]}]}/>
+      </RotationGestureHandler>
     );
   }
 }
@@ -205,11 +249,13 @@ export default class Example extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <DraggableBox/>
+        <TwistableBox/>
       </View>
     );
   }
 }
+
+const BOX_SIZE = 200;
 
 const styles = StyleSheet.create({
   container: {
@@ -220,12 +266,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   box: {
-    width: 150,
-    height: 150,
+    width: BOX_SIZE,
+    height: BOX_SIZE,
     alignSelf: 'center',
     backgroundColor: 'plum',
     margin: 10,
-    zIndex: 200,
   },
 });
 
