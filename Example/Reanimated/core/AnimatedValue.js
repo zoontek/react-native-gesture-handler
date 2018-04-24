7d37f82a55b5f4e6f0172aeccaa7c102a77b8b10
Krzysztof Magiera <krzys@swmansion.com>
2018-04-24 12:41:19 +0200

    Value.js cleanup

--- Example/Reanimated/core/AnimatedValue.js
+++ Example/Reanimated/core/AnimatedValue.js
@@ -5,15 +5,11 @@ function sanitizeValue(value) {
   return value === null || value === undefined ? value : Number(value);
 }
 
-let _uniqueId = 1;
-
 export default class AnimatedValue extends AnimatedNode {
   constructor(value) {
     super({ type: 'value', value: sanitizeValue(value) });
     this._startingValue = this._value = value;
-    this._offset = 0;
     this._animation = null;
-    this._listeners = {};
   }
 
   __detach() {
@@ -28,165 +24,8 @@ export default class AnimatedValue extends AnimatedNode {
     return this._value + this._offset;
   }
 
-  /**
-   * Directly set the value.  This will stop any animations running on the value
-   * and update all the bound properties.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#setvalue
-   */
-  setValue(value) {
-    if (this._animation) {
-      this._animation.stop();
-      this._animation = null;
-    }
-    this._updateValue(
-      value,
-      !this.__isNative /* don't perform a flush for natively driven values */
-    );
-    if (this.__isNative) {
-      NativeAnimatedAPI.setAnimatedNodeValue(this.__getNativeTag(), value);
-    }
-  }
-
-  /**
-   * Sets an offset that is applied on top of whatever value is set, whether via
-   * `setValue`, an animation, or `Animated.event`.  Useful for compensating
-   * things like the start of a pan gesture.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#setoffset
-   */
-  setOffset(offset) {
-    this._offset = offset;
-    if (this.__isNative) {
-      NativeAnimatedAPI.setAnimatedNodeOffset(this.__getNativeTag(), offset);
-    }
-  }
-
-  /**
-   * Merges the offset value into the base value and resets the offset to zero.
-   * The final output of the value is unchanged.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#flattenoffset
-   */
-  flattenOffset() {
-    this._value += this._offset;
-    this._offset = 0;
-    if (this.__isNative) {
-      NativeAnimatedAPI.flattenAnimatedNodeOffset(this.__getNativeTag());
-    }
-  }
-
-  /**
-   * Sets the offset value to the base value, and resets the base value to zero.
-   * The final output of the value is unchanged.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#extractoffset
-   */
-  extractOffset() {
-    this._offset += this._value;
-    this._value = 0;
-    if (this.__isNative) {
-      NativeAnimatedAPI.extractAnimatedNodeOffset(this.__getNativeTag());
-    }
-  }
-
-  /**
-   * Adds an asynchronous listener to the value so you can observe updates from
-   * animations.  This is useful because there is no way to
-   * synchronously read the value because it might be driven natively.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#addlistener
-   */
-  addListener(callback) {
-    const id = String(_uniqueId++);
-    this._listeners[id] = callback;
-    if (this.__isNative) {
-      this._startListeningToNativeValueUpdates();
-    }
-    return id;
-  }
-
-  /**
-   * Unregister a listener. The `id` param shall match the identifier
-   * previously returned by `addListener()`.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#removelistener
-   */
-  removeListener(id) {
-    delete this._listeners[id];
-  }
-
-  /**
-   * Remove all registered listeners.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#removealllisteners
-   */
-  removeAllListeners() {
-    this._listeners = {};
-  }
-
-  /**
-   * Stops any running animation or tracking. `callback` is invoked with the
-   * final value after stopping the animation, which is useful for updating
-   * state to match the animation position with layout.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#stopanimation
-   */
-  stopAnimation(callback) {
-    this.stopTracking();
-    this._animation && this._animation.stop();
-    this._animation = null;
-    callback && callback(this.__getValue());
-  }
-
-  /**
-   * Stops any animation and resets the value to its original.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#resetanimation
-   */
-  resetAnimation(callback) {
-    this.stopAnimation(callback);
-    this._value = this._startingValue;
-  }
-
-  /**
-   * Typically only used internally, but could be used by a custom Animation
-   * class.
-   *
-   * See http://facebook.github.io/react-native/docs/animatedvalue.html#animate
-   */
-  animate(animation) {
-    this._animation && this._animation.stop();
-    this._animation = animation;
-
-    const node = animation.start(this);
-    this.__detach();
-    this.__inputNodes = [node];
-    this.__attach();
-    this.__dangerouslyRescheduleEvaluate();
-  }
-
-  /**
-   * Typically only used internally.
-   */
-  stopTracking() {
-    this._tracking && this._tracking.__detach();
-    this._tracking = null;
-  }
-
-  /**
-   * Typically only used internally.
-   */
-  track(tracking) {
-    this.stopTracking();
-    this._tracking = tracking;
-  }
-
-  _updateValue(value, flush) {
+  _updateValue(value) {
     this._value = value;
     this.__forceUpdateCache(value);
-    for (const key in this._listeners) {
-      this._listeners[key]({ value: this.__getValue() });
-    }
   }
 }
